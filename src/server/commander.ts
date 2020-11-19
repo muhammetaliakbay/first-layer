3d4f1681fe0b5b8815994c182030f2337b231c21
Muhammet Ali AKBAY <ben@muhammetaliakbay.com>
2020-11-19 22:18:48 +0300

    Added feature to let the node connect to neighbour nodes

--- src/server/commander.ts
+++ src/server/commander.ts
@@ -3,7 +3,7 @@ import {SubjectAddress} from "../subject-address";
 import readline from "readline";
 import {parseJSONs} from "../human";
 import {getParameters} from "./parameters";
-import {connect, node} from "./node";
+import {node} from "./node";
 
 const dataListener: DataListener = (subjectAddress, data, hops) => {
     console.log(subjectAddress.toString(), '0x' + data.toString('hex'), hops.length + ' h.');
@@ -34,7 +34,7 @@ rl.on('line', async line => {
 
             if (cmd === 'connect') {
                 const url: string = args[0];
-                const connection = await connect(url);
+                const connection = await node.connect(url);
                 console.log('Connected ' + connection.identity.bytes.toString('hex'));
             } else if (cmd === 'pub') {
                 const address = args[0] instanceof Buffer ? new SubjectAddress(args[0]) : await SubjectAddress.resolve(args[0]);
